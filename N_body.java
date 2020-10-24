238d43d6ff6241f5855f0b55d7f690673505aa11
srtabarestarquinio <49605681+srtabarestarquinio@users.noreply.github.com>
2020-10-24 01:24:47 -0500

    Update N_body.java

--- N_body.java
+++ N_body.java
@@ -1,19 +1,20 @@
 //Rose Tabares - CS245 - Assignment01 - N-Body-Problem
 import java.util.*;
 
-import javax.swing.JPanel;
-
-import javax.swing.JFrame;
-
-import java.awt.Graphics;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.awt.Color;
-import java.io.IOException;
-import java.io.*;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.Timer;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.*;
+
 
 public class N_body extends JPanel implements ActionListener{
 	private String name;
@@ -82,13 +83,24 @@ public class N_body extends JPanel implements ActionListener{
 		}
 
 	}
+	//Timer object: GUI version of the sleep method, good for animation
+	//sleep for 5 seconds
 	Timer tm = new Timer(5, this);
 	int x=0, velx=2;
 
+	//From Tutorial: Making Shapes on JPanel:
 	public void paintComponent(Graphics g){
+		//super b/c from extended JPanel
 		super.paintComponent(g);
 		g.setColor(Color.RED);
-		g.fillOval(x, 100, 50, 50);
+		g.drawOval(x, 100, 50, 50);
+		//bigger coordinate to not over lap the circle
+		g.fillOval(x, 200, 50, 50);
+		//other methods:
+			// g.drawRect(100, 10, 30, 40);
+			// g.fillRect(10, 10, 20, 10);
+			// g.drawLine(10, 10, 30, 70);
+		//start timer, start action listener:
 		tm.start();
 	}
 	public void actionPerformed(ActionEvent e){
@@ -96,6 +108,7 @@ public class N_body extends JPanel implements ActionListener{
 			velx = -velx;
 		}
 		x += velx;
+		//built in method, repaints the figure every 5 seconds
 		repaint();
 	}
 	public void NbodyCreator(String filename) throws IOException{
@@ -117,11 +130,14 @@ public class N_body extends JPanel implements ActionListener{
 		CelestBody body1 = new CelestBody("Earth", 20000, 20, 5, 16, 3000, 20000);
 		System.out.println(body1.giveName());
 
+		//t to reprent JPanel:
 		N_body t = new N_body();
+		//JFrame object
 		JFrame jf = new JFrame();
-		jf.setTitle( "Canvas" );
+		jf.setTitle( "N_body");
 		int t.maxX = t.maxY = 768;
 		jf.setSize( t.maxX , t.maxY ); // Window size defined in the class
+		//adding JPanel onto the frame
 		jf.add(t); // This appears below "setVisible" in the video
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
